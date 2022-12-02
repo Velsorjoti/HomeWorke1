8e4f4c5881ab54ab43af58c4fe92f98d4c8495cd
Velsorjoti <OdzenNedvijiimii@gmail.com>
2022-12-02 15:27:22 +0400

    Задание до сессии с экспертом.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
+public class Main
+{
     public static void main(String[] args) {
-        System.out.println("Привет, мир!");
+        System.out.println("Самостоятельное задание до сессии с экспертом");
+        byte a = 10;
+        float b = 8.50f;
+        float c = 9.45f;
+        boolean delta = (a - b) > (a - c);
+        if(delta) {
+            System.out.println(c + " ближе к " + a);
+        }
+        else {
+            System.out.println(b + " ближе к " + a);
+        }
     }
 }
